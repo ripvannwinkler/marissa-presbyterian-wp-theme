071704b23c9249fb68d8890ca385c1b0e57529fe
Chris Vann <chris@elserinteractive.com>
2020-03-03 13:07:24 -0600

    update npm scripts, move lib/ to dist/

--- functions.php
+++ functions.php
@@ -126,7 +126,7 @@ add_action('tgmpa_register', 'mpc1_register_required_plugins');
 function load_stylesheets()
 {
     $template_dir = get_template_directory_uri();
-    wp_register_style('app-styles', $template_dir . '/lib/app.css', array(), false, "all");
+    wp_register_style('app-styles', $template_dir . '/dist/app.css', array(), false, "all");
     wp_enqueue_style('app-styles');
 }
 
@@ -138,7 +138,7 @@ add_action("wp_enqueue_scripts", "load_stylesheets");
 function load_scripts()
 {
     $template_dir = get_template_directory_uri();
-    wp_register_script("app-script", $template_dir . '/lib/app.js', array(), false, true);
+    wp_register_script("app-script", $template_dir . '/dist/app.js', array(), false, true);
     wp_enqueue_script("app-script");
 }
 
